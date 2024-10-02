9691579a41bdcfb6b601076554f57a64a4487714
אורי ק <orikoj10@gmail.com>
2024-10-02 12:44:56 +0300

    add write to csv file

--- src/flight_file_process/process.py
+++ src/flight_file_process/process.py
@@ -3,9 +3,10 @@ from typing import IO, Dict, List
 import csv
 CSV_NAME = 'flights.csv'
 CSV_FOLDER = 'flights'
+FLIGHT_ID_COLUMN = 'flight ID'
 ARRIVAL_COLUMN = ' Arrival' # TODO fix these
-DEPARTURE_COLUMN = ' Departure' # TODO consider using the field names from the csv file directly
-SUCCESS_COLUMN = ' Success'
+DEPARTURE_COLUMN = ' Departure ' # TODO consider using the field names from the csv file directly
+SUCCESS_COLUMN = ' success'
 ARRIVAL_DEPARTURE_GAP_FOR_SUCCESS = 180
 MAX_SUCCESS = 20  # TODO reorgenize consts
 SUCCESS = 'success'
@@ -16,18 +17,26 @@ def get_file_line_dicts(file: IO) -> List[Dict]:
     return file_dict
 def sort_file_dict(file_dict: List[Dict]):
     # TODO consider to use pandas or datetime
-    file_dict.sort(key=lambda row: get_minutes_from_midnight(row[' Arrival'].strip()))
+    file_dict.sort(key=lambda row: get_minutes_from_midnight(row[' Arrival']))
     return file_dict
 def get_minutes_from_midnight(daytime: str):
+    daytime = daytime.strip()
     return int(daytime[0:2]) * 60 + int(daytime[3:])
 
 def read_csv(file: IO) -> List[Dict]:
     return [row for row in csv.DictReader(file)]
 
+def write_list_to_csv(csv_list: List[Dict], file: IO):
+    writer = csv.DictWriter(file, (FLIGHT_ID_COLUMN,ARRIVAL_COLUMN, DEPARTURE_COLUMN, SUCCESS_COLUMN))
+    writer.writeheader()
+    writer.writerows(csv_list)
 
-def add_success_column(file: IO):
-    file_dict = get_file_line_dicts(file)
-    file_dict = _add_success_column_in_memory(file_dict)
+def add_success_column(file_path: Path):
+    with file_path.open() as file:
+        csv_list = get_file_line_dicts(file)
+        csv_list = _add_success_column_in_memory(csv_list)
+    with file_path.open('w') as file:
+        write_list_to_csv(csv_list, file)
 
 def _add_success_column_in_memory(file_dict: List[Dict]) -> List[Dict]:
     num_success = 0
@@ -42,8 +51,7 @@ def _add_success_column_in_memory(file_dict: List[Dict]) -> List[Dict]:
     return file_dict
 
 def handle_file(file_path: Path):
-    with file_path.open() as f:
-        add_success_column(f)
+    add_success_column(file_path)
 
 
 if __name__ == '__main__':
